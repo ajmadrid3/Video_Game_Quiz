5a823179aba9b7a46ed4b3cd6289198b5973e475
Andrew Madrid <ajmadrid3@miners.utep.edu>
2017-05-02 16:47:28 -0500

    Added data file.

---
+++ data/data.js
@@ -0,0 +1,8 @@
+var data = [
+                {VideoGameName: "Battlefield 1",        VideoGameGenre: "First-person shooter",     VideoGameDev: "DICE",                       ReleaseYear: 2016,  Platform: "Xbox One" },
+                {VideoGameName: "Halo 5: Guardians",    VideoGameGenre: "First-person shooter",     VideoGameDev: "343 Industries",             ReleaseYear: 2015,  Platform: "Xbox One" },
+                {VideoGameName: "Overwatch",            VideoGameGenre: "First-person shooter",     VideoGameDev: "Blizzard Entertainment",     ReleaseYear: 2016,  Platform: "PC"},
+                {VideoGameName: "Portal 2",             VideoGameGenre: "Puzzle-platform",          VideoGameDev: "Valve",                      ReleaseYear: 2011,  Platform: "PC"},
+                {VideoGameName: "Civilization 5",       VideoGameGenre: "Turn-based strategy",      VideoGameDev: "Firaxis Games",              ReleaseYear: 2010,  Platform: "PC"},
+                {VideoGameName: "Destiny",              VideoGameGenre: "First-person shooter",     VideoGameDev: "Bungie",                     ReleaseYear: 2014,  Platform: "Playstation 4"}
+            ];

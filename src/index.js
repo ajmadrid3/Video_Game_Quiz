d559e9ffbdf614e68e22416e987f970da5593b93
Andrew Madrid <ajmadrid3@miners.utep.edu>
2017-05-03 09:24:23 -0500

    Added more consoles to slotvalue.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ var data = [
                 {VideoGameName: "Overwatch",            VideoGameGenre: "First-person shooter",     VideoGameDev: "Blizzard Entertainment",     ReleaseYear: 2016,  Platform: "PC"},
                 {VideoGameName: "Portal 2",             VideoGameGenre: "Puzzle-platform",          VideoGameDev: "Valve",                      ReleaseYear: 2011,  Platform: "PC"},
                 {VideoGameName: "Civilization 5",       VideoGameGenre: "Turn-based strategy",      VideoGameDev: "Firaxis Games",              ReleaseYear: 2010,  Platform: "PC"},
-                {VideoGameName: "Destiny",              VideoGameGenre: "First-person shooter",     VideoGameDev: "Bungie",                     ReleaseYear: 2014,  Platform: "Playstation 4"},
+                {VideoGameName: "Destiny",              VideoGameGenre: "First-person shooter",     VideoGameDev: "Bungie",                     ReleaseYear: 2014,  Platform: "PlayStation 4"},
                 {VideoGameName: "Pokemon Red",          VideoGameGenre: "Role-playing",             VideoGameDev: "Game Freak",                 ReleaseYear: 1996,  Platform: "Game Boy"},
                 {VideoGameName: "Super Mario 64",       VideoGameGenre: "Platformer",               VideoGameDev: "Nintendo",                   ReleaseYear: 1996,  Platform: "Nintendo 64"},
                 {VideoGameName: "The Legend of Zelda",  VideoGameGenre: "Action-adventure",         VideoGameDev: "Nintendo",                   ReleaseYear: 1986,  Platform: "Nintendo Entertainment System"}

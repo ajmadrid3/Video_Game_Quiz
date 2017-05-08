39f3626d28d147a76997e098c814cc97a7130422
Andrew Madrid <ajmadrid3@miners.utep.edu>
2017-05-08 12:52:58 -0500

    Updated data for games in the 70s.
    Deleted previous data due to there beig no publisher.

--- src/index.js
+++ src/index.js
@@ -8,24 +8,15 @@ var APP_ID = undefined;
 
 
 var data = [
-                {VideoGameName: "Pong",                                 VideoGameGenre: "Sports",                   VideoGameDev: "Atari",                      VideoGamePub: "Atari",          ReleaseYear: 1972,  Platform: "Arcade"}
-                /*{VideoGameName: "Battlefield 1",                        VideoGameGenre: "First-person shooter",     VideoGameDev: "DICE",                       ReleaseYear: 2016,  Platform: "Xbox One" },
-                {VideoGameName: "Halo 5: Guardians",                    VideoGameGenre: "First-person shooter",     VideoGameDev: "343 Industries",             ReleaseYear: 2015,  Platform: "Xbox One" },
-                {VideoGameName: "Overwatch",                            VideoGameGenre: "First-person shooter",     VideoGameDev: "Blizzard Entertainment",     ReleaseYear: 2016,  Platform: "PC"},
-                {VideoGameName: "Portal 2",                             VideoGameGenre: "Puzzle-platform",          VideoGameDev: "Valve",                      ReleaseYear: 2011,  Platform: "PC"},
-                {VideoGameName: "Civilization 5",                       VideoGameGenre: "Turn-based strategy",      VideoGameDev: "Firaxis Games",              ReleaseYear: 2010,  Platform: "PC"},
-                {VideoGameName: "Destiny",                              VideoGameGenre: "First-person shooter",     VideoGameDev: "Bungie",                     ReleaseYear: 2014,  Platform: "PlayStation 4"},
-                {VideoGameName: "Pokemon Red",                          VideoGameGenre: "Role-playing",             VideoGameDev: "Game Freak",                 ReleaseYear: 1996,  Platform: "Game Boy"},
-                {VideoGameName: "Super Mario 64",                       VideoGameGenre: "Platformer",               VideoGameDev: "Nintendo",                   ReleaseYear: 1996,  Platform: "Nintendo 64"},
-                {VideoGameName: "The Legend of Zelda",                  VideoGameGenre: "Action-adventure",         VideoGameDev: "Nintendo",                   ReleaseYear: 1986,  Platform: "Nintendo Entertainment System"},
-                {VideoGameName: "Pong",                                 VideoGameGenre: "Sports",                   VideoGameDev: "Atari",                      ReleaseYear: 1972,  Platform: "Arcade"},
-                {VideoGameName: "Halo: Combat Evolved",                 VideoGameGenre: "First-person shoorter",    VideoGameDev: "Bungie",                     ReleaseYear: 2001,  Platform: "Xbox"},
-                {VideoGameName: "Sonic Adventure",                      VideoGameGenre: "Platformer",               VideoGameDev: "Sonic Team",                 ReleaseYear: 1999,  Platform: "Dreamcast"},
-                {VideoGameName: "Call of Duty: Modern Warefare 2",      VideoGameGenre: "First-person shooter",     VideoGameDev: "Infinity Ward",              ReleaseYear: 2009,  Platform: "Xbox 360"},
-                {VideoGameName: "Super Mario World",                    VideoGameGenre: "Platformer",               VideoGameDev: "Nintendo",                   ReleaseYear: 1991,  Platform: "Super Nintendo Entertainment System"},
-                {VideoGameName: "Donkey Kong Country",                  VideoGameGenre: "Platformer",               VideoGameDev: "Rare",                       ReleaseYear: 1994,  Platform: "Super Nintendo Entertainment System"},
-                {VideoGameName: "Grand Theft Auto: San Andreas",        VideoGameGenre: "Action-adventure",         VideoGameDev: "Rockstar",                   ReleaseYear: 2004,  Platform: "PlayStation 2"}
-            */];
+                {VideoGameName: "Pong",                                 VideoGameGenre: "Sports",                       VideoGameDev: "Atari",                      VideoGamePub: "Atari",          ReleaseYear: 1972,  Platform: "Arcade"},
+                {VideoGameName: "Space Race",                           VideoGameGenre: "Racing",                       VideoGameDev: "Atari",                      VideoGamePub: "Atari",          ReleaseYear: 1973,  Platform: "Arcade"},
+                {VideoGameName: "Tank",                                 VideoGameGenre: "Maze",                         VideoGameDev: "Kee Games",                  VideoGamePub: "Kee Games",      ReleaseYear: 1974,  Platform: "Arcade"},
+                {VideoGameName: "Gun Fight",                            VideoGameGenre: "Shoot 'em up",                 VideoGameDev: "Taito",                      VideoGamePub: "Midway",         ReleaseYear: 1975,  Platform: "Arcade"},
+                {VideoGameName: "Breakout",                             VideoGameGenre: "Arcade",                       VideoGameDev: "Atari",                      VideoGamePub: "Atari",          ReleaseYear: 1976,  Platform: "Arcade"},
+                {VideoGameName: "Combat",                               VideoGameGenre: "Multi-directional shooter",    VideoGameDev: "Atari",                      VideoGamePub: "Atari",          ReleaseYear: 1977,  Platform: "Atari 2600"},
+                {VideoGameName: "Space Invaders",                       VideoGameGenre: "Shoot 'em up",                 VideoGameDev: "Taito",                      VideoGamePub: "Midway",         ReleaseYear: 1978,  Platform: "Arcade"},
+                {VideoGameName: "Galaxian",                             VideoGameGenre: "Shoot 'em up",                 VideoGameDev: "Namco",                      VideoGamePub: "Midway",         ReleaseYear: 1979,  Platform: "Arcade"}
+            ];
 
 //=========================================================================================================================================
 // Messages: 
